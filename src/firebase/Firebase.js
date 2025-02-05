c06eda478fba45b9b81b2b9c138a06a39cd10e4c
younus-always <younusofficial443@gmail.com>
2025-02-05 15:20:11 +0600

    SkillSpring .env file setup

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfjkdep_wtvLXZN_F87vJ4O_tMsMk0PKs",
-  authDomain: "winter-shield.firebaseapp.com",
-  projectId: "winter-shield",
-  storageBucket: "winter-shield.firebasestorage.app",
-  messagingSenderId: "550454232398",
-  appId: "1:550454232398:web:5255500f45a9145d9cca1f"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 
